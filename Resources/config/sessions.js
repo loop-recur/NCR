49e06af4de8c531083fc7c4b8759a0651c8650fc
Taylor Smith <tjsmith19@gmail.com>
2011-12-26 12:43:28 -0500

    sess

--- Resources/config/sessions.js
+++ Resources/config/sessions.js
@@ -1,14 +1,14 @@
 Config['Sessions'] = [
-{
-    created_at : "2011-12-07T21:14:55Z",
-    description : "We'll have this info in a few.",
-    end_time : "1/11/2012 21:45:00",
-    id : 1,
-    speaker_id : 0,
-    start_time : "12/07/2011 20:30:00",
-    title : "Loading initial data...",
-    updated_at : "2011-12-09T21:17:16Z"
-}
-
+	{
+	    created_at : "2011-12-07T21:14:55Z",
+			title : "Loading initial data...",    
+			description : "We'll have this info in a few.",
+	    start_time : "1/11/2012 20:30:00",
+			end_time : "1/11/2012 21:45:00",
+	    id : 1,
+	    speaker_id : 0,
+	    
+			location : "Texas Ballroom",
+	    updated_at : "2011-12-09T21:17:16Z"
+	},
 ]
-
