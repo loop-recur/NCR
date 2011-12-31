50d6b0297e28778ebb3c018de646125073af4e05
Brian <brian@trnsfr.com>
2011-12-30 17:22:34 -0800

    new window for images with fix

--- Resources/views/maps/index.js
+++ Resources/views/maps/index.js
@@ -7,29 +7,33 @@ Views.maps.index = function(win) {
 	
 	subtabs.delegate = {
 		getContent : function(view, e) {
-			var scrollview = Ti.UI.createScrollView({
-				contentWidth:"auto",
-				contentHeight:'auto',
-				width:'100%',
-				height:'100%',
-				showHorizontalScrollIndicator:true,
-				showVerticalScrollIndicator:true
-			});
-			
-			var map_image = Ti.UI.createImageView({
-				image:'images/maps/'+e.source.id.replace(/\s+/g, '').toLowerCase()+'.png'
-			});
-			
-			scrollview.add(map_image);
+			var makeView = function() {
+				var scrollview = Ti.UI.createScrollView({
+					contentWidth:"auto",
+					contentHeight:'auto',
+					width:'100%',
+					height:'100%',
+					showHorizontalScrollIndicator:true,
+					showVerticalScrollIndicator:true
+				});
+
+				var map_image = Ti.UI.createImageView({
+					image:'images/maps/'+e.source.id.replace(/\s+/g, '').toLowerCase()+'.png'
+				});
+				
+				scrollview.add(map_image);
+				
+				return scrollview;
+			}
 			
 			map_image.addEventListener('click', function(){
 				var win = Ti.UI.createWindow({fullscreen: true});
-				win.add(scrollview);
+				win.add(makeView());
 				win.open();
 			});
 			
 			
-			view.add(scrollview);
+			view.add(makeView());
 		}
 	}
 }
