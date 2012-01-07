d8b3c8299323dae61c9aecee4a01fe503dc4d87e
Brian <brian@trnsfr.com>
2012-01-06 17:39:34 -0800

    fixing db thing

--- Resources/lib/framework/lib/schema_load.js
+++ Resources/lib/framework/lib/schema_load.js
@@ -2,6 +2,16 @@ SchemaLoad = (function() {
 	
 var createDb = function(config) {
 	if(config.redo) map(App.db.drop, keys(Schema));
+	
+	try {
+		makeTables();
+	} catch(e) {
+		map(App.db.drop, keys(Schema));
+		makeTables();
+	}
+}
+
+var makeTables = function() {
 	for(table in Schema) App.db.create(table, Schema[table]);	
 }
 
